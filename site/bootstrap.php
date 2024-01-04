4b78e9108518db0e2c2f763e92a233a2b762c8d6
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2024-01-04 21:46:28 +0100

    Modified bootstrap.php

---
+++ site/bootstrap.php
@@ -0,0 +1,12 @@
+<?php 
+require_once("../db/database.php");
+require_once("./utils/functions.php");
+$server = "localhost";
+$username = "root";
+$pwd = "";
+$dbname = "revly_db";
+$port = 3306;
+
+$dbh = new DatabaseHelper($server, $username, $pwd, $dbname, $port);
+define("UPLOAD_DIR", "./upload/");
+?>
