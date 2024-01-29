9197aaccf3f17a7e76ca665d17f10a0496d66740
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2024-01-29 22:21:51 +0100

    Refactoring of login-form.php

--- site/template/page-top.php
+++ site/template/page-top.php
@@ -2,46 +2,50 @@
     <nav class="navbar">
         <div class="container-fluid">
             <a class="navbar-brand" href="home.php">REVLY</a>
-            <div class="d-flex">
-                <a class="nav-link active" aria-current="page" href="research-user.php">
-                    <i class="bi bi-search"></i>
+            <ul class="d-flex">
+                <li>
+                    <a class="nav-link active" href="research-user.php">
+                        <i class="bi bi-search"></i>
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link active" href="settings.php">
+                        <i class="bi bi-toggles"></i>
                 </a>
-                <a class="nav-link active" aria-current="page" href="settings.php">
-                    <i class="bi bi-toggles"></i>
-                </a>
-            </div>
+                </li>
+            </ul>
         </div>
     </nav>
 </header>
 
 <div class="row">
-    <div class="col-md-1"></div>
-    <div class="col-md-10">
+    <div class="col-1"></div>
+    <div class="col-10">
         <div class="row text-center">
             <nav class="navbar bg-body-tertiary">
                 <div class="col-3">
-                    <a class="nav-link active" aria-current="page" href="home.php">
+                    <a class="nav-link active" href="home.php">
                         <i class="bi bi-house" alt="Home"></i>
                     </a>
                 </div>
                 <div class="col-3">
-                    <a class="nav-link active" aria-current="page" href="notifications.php">
+                    <a class="nav-link active" href="notifications.php">
                         <i class="bi bi-bell"></i>
                     </a>
                 </div>
                 <div class="col-3">
-                    <a class="nav-link active" aria-current="page" href="create-post.php">
+                    <a class="nav-link active" href="create-post.php">
                         <i class="bi bi-plus-square"></i>
                     </a>
                 </div>
                 <div class="col-3">
-                    <a class="nav-link active" aria-current="page" href="myprofile.php">
+                    <a class="nav-link active" href="myprofile.php">
                         <i class="bi bi-person-circle"></i>
                     </a>
                 </div>
             </nav>
         </div>
     </div>
-    <div class="col-md-1"></div>
+    <div class="col-1"></div>
 </div>
 
