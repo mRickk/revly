c098741e00058e8c9e1c1b16a22a9cd04625eaaa
Juri Guglielmi - juri.guglielmi@studio.unibo.it <juri.guglielmi@studio.unibo.it>
2024-02-05 18:15:12 +0100

    MODIFICA AI PHP PER CONTROLLO SE GET O POST

--- site/deletePost.php
+++ site/deletePost.php
@@ -2,7 +2,7 @@
 require_once("bootstrap.php");
 session_start();
 
-if (isUserLoggedIn()) {
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
     $res = $dbh->deletePost($_GET["idPost"]);
     header("Location: profile.php?username=" . $_SESSION["username"]);
 }
--- site/search.php
+++ site/search.php
@@ -1,7 +1,7 @@
 <?php
 require_once("bootstrap.php");
 
-if (isset($_GET['q'])) {
+if (isset($_GET['q']) && $_SERVER["REQUEST_METHOD"] === "GET" ) {
     $searchTerm = $_GET['q'];
     $suggestions = getTaggableSuggestions($searchTerm, $dbh);
     echo json_encode($suggestions);
