0136ae1204942062aeada739a52c61dff78b71f3
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2024-02-05 17:17:57 +0100

    Refactoring of main-settings.php

--- site/template/main-settings.php
+++ site/template/main-settings.php
@@ -1,71 +1,194 @@
 <?php $user = $templateParams["user"]; ?>
-<div class="settings">
-    <div class="account-settings">
-        <h2>Account</h2>
-        <div class="personal-details">
+<section class="d-flex flex-column min-vh-100">
+    <div class="settings-section flex-grow-1 bg-body-secondary bg-opacity-75 mt-3 p-2">
+        <div class="row gx-1">
+            <h2>Settings</h2>
+        </div>
+        <div class="row gx-1 mb-2">
+            <h3>Account</h3>
             <a href="change-personal-details.php">
-                <i class="bi bi-person-fill-gear"></i>
-                <p>Change personal details</p>
-                <i class="bi bi-caret-right"></i>
+                <div class="row gx-2">
+                    <div class="d-flex align-items-center">
+                        <div class="col-1"></div>
+                        <div class="col-1 me-1">
+                            <i class="bi bi-person-gear fs-2"></i>
+                        </div>
+                        <div class="col-9">
+                            <h4 class="m-0">Change personal details</h4>
+                        </div>
+                        <div class="col-1">
+                            <i class="bi bi-caret-right fs-2"></i>  
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="change-photo-bio.php">
+                <div class="row gx-2">
+                    <div class="d-flex align-items-center">
+                        <div class="col-1"></div>
+                        <div class="col-1 me-1">
+                            <i class="bi bi-image fs-2"></i>
+                        </div>
+                        <div class="col-9">
+                            <h4 class="m-0">Change photo & bio</h4>
+                        </div>
+                        <div class="col-1">
+                            <i class="bi bi-caret-right fs-2"></i> 
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="change-password.php">
+                <div class="row gx-2">
+                    <div class="d-flex align-items-center">
+                        <div class="col-1"></div>
+                        <div class="col-1 me-1">
+                            <i class="bi bi-key fs-2"></i>
+                        </div>
+                        <div class="col-9">
+                            <h4 class="m-0">Change password</h4>
+                        </div>
+                        <div class="col-1">
+                            <i class="bi bi-caret-right fs-2"></i> 
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="index.php">
+                <div class="row gx-2">
+                    <div class="col-1"></div>
+                    <div class="col-11">
+                        <p class="text-danger m-1">Logout from this account</p>
+                    </div>
+                </div>
+            </a>
+            <a href="index.php">
+                <div class="row gx-2">
+                    <div class="col-1"></div>
+                    <div class="col-11">
+                        <p class="text-danger m-1">Delete this account</p><?php //TODO: implementare delete ?>
+                    </div>
+                </div>
             </a>
         </div>
+        <div class="row gx-1 mb-2">
+            <h3>Notifications</h3>
+            <div class="row gx-2">
+                <div class="d-flex align-items-center">
+                    <div class="col-1"></div>
+                    <div class="col-1 me-1">
+                        <i class="bi bi-chat-left-heart fs-2"></i>
+                    </div>
+                    <div class="col-8">
+                        <h4 class="m-0">Likes</h4>
+                    </div>
+                    <div class="col-2">
+                    <?php if ($user["notifyLikes"]):?>
+                        <i class="bi bi-toggle-on fs-1 revly-primary-color"></i>
+                    <?php else:?>
+                        <i class="bi bi-toggle-off fs-1 text-secondary"></i>
+                    <?php endif;?>
+                    </div>
+                </div>
+            </div>
+            <div class="row gx-2">
+                <div class="d-flex align-items-center">
+                    <div class="col-1"></div>
+                    <div class="col-1 me-1">
+                        <i class="bi bi-chat-square-text fs-2"></i>
+                    </div>
+                    <div class="col-8">
+                        <h4 class="m-0">Comments</h4>
+                    </div>
+                    <div class="col-2">
+                    <?php if ($user["notifyComments"]):?>
+                        <i class="bi bi-toggle-on fs-1 revly-primary-color"></i>
+                    <?php else:?>
+                        <i class="bi bi-toggle-off fs-1 text-secondary"></i>
+                    <?php endif;?>
+                    </div>
+                </div>
+            </div>
+            <div class="row gx-2">
+                <div class="d-flex align-items-center">
+                    <div class="col-1"></div>
+                    <div class="col-1 me-1">
+                        <i class="bi bi-tag fs-2"></i>
+                    </div>
+                    <div class="col-8">
+                        <h4 class="m-0">Tags</h4>
+                    </div>
+                    <div class="col-2">
+                    <?php if ($user["notifyTags"]):?>
+                        <i class="bi bi-toggle-on fs-1 revly-primary-color"></i>
+                    <?php else:?>
+                        <i class="bi bi-toggle-off fs-1 text-secondary"></i>
+                    <?php endif;?>
+                    </div>
+                </div>
+            </div>
+            <div class="row gx-2">
+                <div class="d-flex align-items-center">
+                    <div class="col-1"></div>
+                    <div class="col-1 me-1">
+                        <i class="bi bi-person-add fs-2"></i>
+                    </div>
+                    <div class="col-8">
+                        <h4 class="m-0">Follows</h4>
+                    </div>
+                    <div class="col-2">
+                    <?php if ($user["notifyFollows"]):?>
+                        <i class="bi bi-toggle-on fs-1 revly-primary-color"></i>
+                    <?php else:?>
+                        <i class="bi bi-toggle-off fs-1 text-secondary"></i>
+                    <?php endif;?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<div class="settings">
+    <div class="account-settings">
+        
         <div class="photo-bio">
-        <a href="change-photo-bio.php">
-            <i class="bi bi-image"></i>
-            <p>Change photo & bio</p>
-            <i class="bi bi-caret-right"></i>
+        <a href="">
+            
+            <p></p>
+            
         </a>
         </div>
         <div class="password">
-            <a href="change-password.php">
-                <i class="bi bi-key"></i>
-                <p>Change password</p>
-                <i class="bi bi-caret-right"></i>
+            <a href="">
+                
+                <p></p>
+                
             </a>
         </div>
         <div class="logout">
-            <p><a href="index.php">Logout from this account</a></p>
-        </div>
-        <div class="delete">
-            <p><a href="index.php">Delete this account</a></p><?php //TODO: implementare delete ?>
+            <p></p>
         </div>
     </div>
     <div class="notifications-settings">
         <h2>Notifications</h2>
         <div class="likes">
-            <i class="bi bi-chat-left-heart"></i>
+            
             <p>Likes</p>
-            <?php if ($user["notifyLikes"]):?>
-                <i class="bi bi-toggle-on"></i>
-            <?php else:?>
-                <i class="bi bi-toggle-off"></i>
-            <?php endif;?>
+            
         </div>
         <div class="comments">
-            <i class="bi bi-chat-square-text"></i>
+            <i class=""></i>
             <p>Comments</p>
-            <?php if ($user["notifyComments"]):?>
-                <i class="bi bi-toggle-on"></i>
-            <?php else:?>
-                <i class="bi bi-toggle-off"></i>
-            <?php endif;?>
+            
         </div>
         <div class="tags">
-            <i class="bi bi-tag"></i>
+            <i class=""></i>
             <p>Tags</p>
-            <?php if ($user["notifyTags"]):?>
-                <i class="bi bi-toggle-on"></i>
-            <?php else:?>
-                <i class="bi bi-toggle-off"></i>
-            <?php endif;?>
         </div>
         <div class="follows">
             <p>Follows</p>
-            <?php if ($user["notifyFollows"]):?>
-                <i class="bi bi-toggle-on"></i>
-            <?php else:?>
-                <i class="bi bi-toggle-off"></i>
-            <?php endif;?>
         </div>
     </div>
 </div>
