8ec0d6869b73ee90ba4e54aebe2b0c67de245dc8
Juri Guglielmi <juri.guglielmi@studio.unibo.it>
2024-02-06 17:10:31 +0100

    micro change

--- site/research-user.php
+++ site/research-user.php
@@ -2,10 +2,11 @@
 require("bootstrap.php");
 session_start();
 
-$templateParams["title"] = "Search";
-$templateParams["top-template"] = "focus-top.php";
-$templateParams["main-template"] = "search-form.php";
-$templateParams["js"] = ["js/search.js", "js/back.js"];
-    
+if(isUserLoggedIn()){
+    $templateParams["title"] = "Search";
+    $templateParams["top-template"] = "focus-top.php";
+    $templateParams["main-template"] = "search-form.php";
+    $templateParams["js"] = ["js/search.js", "js/back.js"];
+}
 require("template/base.php");
 ?>
